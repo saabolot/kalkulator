4e76643958e21c01bfaebf88b744ef5e3f6f27fe
saabolot <97003103+saabolot@users.noreply.github.com>
2022-01-09 22:51:13 +0100

    wersja 2

--- kalkulator.py
+++ kalkulator.py
@@ -1,36 +1,34 @@
-
 def suma(zmienna_x,zmienna_y):
     return(zmienna_x + zmienna_y)
-print(suma(zmienna_x,zmienna_y))
-
 def roznica(zmienna_x,zmienna_y):
     return(zmienna_x - zmienna_y)
-print(roznica(zmienna_x,zmienna_y))
-
 def iloczyn(zmienna_x,zmienna_y):
     return(zmienna_x * zmienna_y)
-print(iloczyn(zmienna_x,zmienna_y))
-
 def iloraz(zmienna_x,zmienna_y):
     return(zmienna_x / zmienna_y)
-print(iloraz(zmienna_x,zmienna_y))
 
 if __name__ == "__main__":
     zmienna_x = int(input("podaj zmienna_x: "))
     zmienna_y = int(input("podaj zmienna_y: "))
-    dzialanie = input("""
+    dzialanie = int(input("""
 Podaj liczbę aby wybrać odpowiednie działanie:
 1 - Dodawanie
 2 - Odejmowanie
 3 - Mnozenie
 4 - Dzielenie
-""")   
+"""))   
    
     if dzialanie == 1:
         suma(zmienna_x,zmienna_y)
-    if dzialanie == 2:
+        print(f"Wynik dodawania: {suma(zmienna_x,zmienna_y)}")
+    elif dzialanie == 2:
         roznica(zmienna_x,zmienna_y)
-    if dzialanie == 3:
+        print(f"Wyniki odejmowania: {roznica(zmienna_x,zmienna_y)}")
+    elif dzialanie == 3:
         iloczyn(zmienna_x,zmienna_y)
-    if dzialanie == 4:
+        print(f"Wynik mnozenia: {iloczyn(zmienna_x,zmienna_y)}")
+    elif dzialanie == 4:
         iloraz(zmienna_x,zmienna_y)
+        print(f"Wynik dzielenia: {iloraz(zmienna_x,zmienna_y)}")
+    else:
+        print("Nieprawidłowy wybór działania")
